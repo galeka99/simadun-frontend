0e19687271e9cf8318944dd3a28cc8ccd1a58f0f
Galang <galange99@gmail.com>
2025-10-17 18:59:00 +0700

    feat: add some landing page update

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,7 +1,24 @@
 <script setup lang="ts">
-//
+import { RouterLink } from 'vue-router'
 </script>
 
 <template>
-  <div></div>
+  <div class="flex flex-col items-center w-screen h-screen bg-indigo-50 overflow-hidden">
+    <nav class="flex flex-row justify-center bg-indigo-500 text-white w-full h-12">
+      <div class="flex flex-row justify-between w-full max-w-[1200px] h-full">
+        <div class="flex flex-row items-center py-2 px-5">
+          <img src="@/assets/images/kota_semarang.png" alt="logo kota semarang" class="h-8 w-auto" />
+          <span class="ms-3 text-lg font-bold self-center">SiMaDun</span>
+        </div>
+        <router-link to="/login" class="btn btn-primary rounded-none">
+          <span>Masuk</span>
+          <span class="material-symbols-outlined ms-3">chevron_right</span>
+        </router-link>
+      </div>
+    </nav>
+    <main class="flex flex-col items-center bg-white w-full max-w-[1200px] min-h-screen shadow-lg p-5">
+      <span class="text-xl font-semibold">Jadwal Ketersediaan Gedung</span>
+      <div class="divider my-3"></div>
+    </main>
+  </div>
 </template>
